8be6cc7ed8b50cceca3a3b6797ef9bf753816b54
Jagan <Jaganrajagopalme@gmail.com>
2020-04-21 19:36:48 +0530

    Add the docker files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetcorewithDockercontainers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
